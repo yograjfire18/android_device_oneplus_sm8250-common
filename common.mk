41195d1ae86a7a7c7cf7a7a20d72f0c7e247d391
yograjfire18 <yograjfire18@gmail.com>
2025-03-04 17:04:49 +0530

    sm8250-common: Drop KeyHandler

--- common.mk
+++ common.mk
@@ -28,11 +28,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     AntHalService-Soong
 
-# Alert slider
-PRODUCT_PACKAGES += \
-    KeyHandler \
-    tri-state-key-calibrate
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
